4d14b56bbd4b658579d0107ea2c40ff56d34c99b
currysidd <159699021+currysidd@users.noreply.github.com>
2024-06-18 18:55:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function takeSnapshot(){
+
+}
+
+function preload(){
+nose=loadImage("mustache.png")
+}
+
+function setup(){
+
+    canvas=createCanvas(300,300)
+    canvas.center()
+}
+
+function draw(){
+image(nose,200,200,90,50)
+}
+
