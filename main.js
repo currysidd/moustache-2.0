fb3cd63cd2de7b33a3c5fda733d5e08790110368
currysidd <159699021+currysidd@users.noreply.github.com>
2024-07-23 08:33:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -10,9 +10,24 @@ function setup(){
 
     canvas=createCanvas(300,300)
     canvas.center()
+    video=createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+    poseNet=ml5.poseNet(video,modelLoaded)
+    poseNet.on("pose", gotPoses)
 }
 
 function draw(){
-image(nose,200,200,90,50)
+    image(video,0,0,300,300)
+image(nose,250,250,50,50)
 }
 
+function modelLoaded(){
+    console.log("poseNet is ready");
+}
+
+function gotPoses(result){
+    if (result.length>0) {
+        console.log(result);
+    }
+}
